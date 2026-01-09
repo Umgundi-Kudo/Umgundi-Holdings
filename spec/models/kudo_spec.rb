632566910b7a49a42ae0c4104db0d127eb31704e
Mihle14 <mihlentsaluba14@gmail.com>
2026-01-09 12:33:35 +0200

    kudo

--- spec/models/kudo_spec.rb
+++ spec/models/kudo_spec.rb
@@ -1,113 +1,113 @@
-# require 'rails_helper'
+require 'rails_helper'
 
-# RSpec.describe Kudo do
-#     let(:sender) do
-#         User.create!(
-#             username: "sender",
-#             email: 'sender@gmail.com',
-#             password: '12345678910',
-#             password_confirmation: '12345678910'
-#         )
-#     end
+RSpec.describe Kudo, type: :model do
+    let(:sender) do
+        User.create!(
+            username: "sender",
+            email: 'sender@gmail.com',
+            password: '12345678910',
+            password_confirmation: '12345678910'
+        )
+    end
 
-#     let(:receiver) do
-#         User.create!(
-#             username: "receiver",
-#             email: 'receiver@gmail.com',
-#             password: '2468101214',
-#             password_confirmation: '2468101214'
-#         )
-#     end
-
-
-#     it 'creates a valid kudo' do
-#         kudo = Kudo.new(
-#             sender: sender,
-#             receiver: receiver,
-#             category: 'Teamwork',
-#             message: 'Great job!'
-#         )
-#     end
-
-#     it 'creates a valid kudo' do
-#         kudo = Kudo.new(
-#             sender: sender,
-#             receiver: receiver,
-#             category: 'Teamwork',
-#             message: 'Great job!'
-#         )
-
-#         expect(kudo).to be_valid
-#     end
+    let(:receiver) do
+        User.create!(
+            username: "receiver",
+            email: 'receiver@gmail.com',
+            password: '2468101214',
+            password_confirmation: '2468101214'
+        )
+    end
 
 
+    it 'creates a valid kudo' do
+        kudo = Kudo.new(
+            sender: sender,
+            receiver: receiver,
+            category: 'Teamwork',
+            message: 'Great job!'
+        )
+    end
 
-#     it 'is invalid without a sender' do
-#         kudo = Kudo.new(
-#             receiver: receiver,
-#             category: 'Teamwork',
-#             message: 'Nice work'
-#         )
+    it 'creates a valid kudo' do
+        kudo = Kudo.new(
+            sender: sender,
+            receiver: receiver,
+            category: 'Teamwork',
+            message: 'Great job!'
+        )
 
-#         expect(kudo).not_to be_valid
-#     end
-
-#     it 'is invalid without a receiver' do
-#         kudo = Kudo.new(
-#             sender: sender,
-#             category: 'Teamwork',
-#             message: 'Nice work'
-#         )
-
-#         expect(kudo).not_to be_valid
-#     end
-
-#     it 'is invalid without a category' do
-#         kudo = Kudo.new(
-#             sender: sender,
-#             receiver: receiver,
-#             message: 'Nice work'
-#         )
-
-#         expect(kudo).not_to be_valid
-#     end
-
-
-#     it 'persists a kudo to the database' do
-#         sender = User.create!(
-#             username: "sender",
-#             email: "sender@example.com",
-#             password: "password123",
-#             password_confirmation: "password123"
-#         )
-
-#         receiver = User.create!(
-#             username: "receiver",
-#             email: "receiver@example.com",
-#             password: "password123",
-#             password_confirmation: "password123"
-#         )
-
-#         expect {
-#             Kudo.create!(
-#             sender: sender,
-#             receiver: receiver,
-#             category: 'Leadership',
-#             message: 'Excellent leadership!'
-#             )
-#         }.to change { Kudo.count }.by(1)
-#     end
+        expect(kudo).to be_valid
+    end
 
 
 
-#     it 'is invalid if message is too long' do
-#         kudo = Kudo.new(
-#             sender: sender,
-#             receiver: receiver,
-#             message: 'a' * 501,
-#             category: 'Teamwork'
-#             )
+    it 'is invalid without a sender' do
+        kudo = Kudo.new(
+            receiver: receiver,
+            category: 'Teamwork',
+            message: 'Nice work'
+        )
 
-#         expect(kudo).not_to be_valid
-#     end
-# end
+        expect(kudo).not_to be_valid
+    end
+
+    it 'is invalid without a receiver' do
+        kudo = Kudo.new(
+            sender: sender,
+            category: 'Teamwork',
+            message: 'Nice work'
+        )
+
+        expect(kudo).not_to be_valid
+    end
+
+    it 'is invalid without a category' do
+        kudo = Kudo.new(
+            sender: sender,
+            receiver: receiver,
+            message: 'Nice work'
+        )
+
+        expect(kudo).not_to be_valid
+    end
+
+
+    it 'persists a kudo to the database' do
+        sender = User.create!(
+            username: "sender",
+            email: "sender@example.com",
+            password: "password123",
+            password_confirmation: "password123"
+        )
+
+        receiver = User.create!(
+            username: "receiver",
+            email: "receiver@example.com",
+            password: "password123",
+            password_confirmation: "password123"
+        )
+
+        expect {
+            Kudo.create!(
+            sender: sender,
+            receiver: receiver,
+            category: 'Leadership',
+            message: 'Excellent leadership!'
+            )
+        }.to change { Kudo.count }.by(1)
+    end
+
+
+
+    it 'is invalid if message is too long' do
+        kudo = Kudo.new(
+            sender: sender,
+            receiver: receiver,
+            message: 'a' * 501,
+            category: 'Teamwork'
+            )
+
+        expect(kudo).not_to be_valid
+    end
+end
