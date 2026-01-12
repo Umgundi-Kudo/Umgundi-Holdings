2d85f5f8cf17c16e9bf06c8f993ad267d46c9ce2
Konke Gulwa <konkegulwa02@gmail.com>
2026-01-12 08:36:51 +0200

    Configure Rails generators to use UUID primary keys by default

--- config/application.rb
+++ config/application.rb
@@ -22,12 +22,12 @@ module UmgundiHoldings
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.1
 
-    # Please, add to the `ignore` list any other `lib` subdirectories that do
-    # not contain `.rb` files, or that should not be reloaded or eager loaded.
-    # Common ones are `templates`, `generators`, or `middleware`, for example.
-    config.autoload_lib(ignore: %w(assets tasks))
+    # Configure autoloading for lib/, ignoring non-Ruby directories
+    config.autoload_lib(ignore: %w[assets tasks])
 
-    # Generators configuration (RSpec + UUIDs) 
+    
+    # - Use UUIDs as primary keys by default
+    # - Use RSpec for testing
     config.generators do |g|
       g.orm :active_record, primary_key_type: :uuid
 
@@ -38,10 +38,7 @@ module UmgundiHoldings
                        routing_specs: false
     end
 
-    # Configuration for the application, engines, and railties goes here.
-    #
-    # These settings can be overridden in specific environments using the files
-    # in config/environments, which are processed later.
+    # Additional configuration can be placed here
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
