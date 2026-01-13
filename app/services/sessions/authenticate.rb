6afc35cf7bb00313efaf2f40ff268db8d082a528
Konke Gulwa <konkegulwa02@gmail.com>
2026-01-13 17:31:38 +0200

    Make Sessions::Authenticate accept email or username

--- app/services/sessions/authenticate.rb
+++ app/services/sessions/authenticate.rb
@@ -1,16 +1,23 @@
 module Sessions
   class Authenticate
-    def self.call(username:, password:)
-      new(username, password).call
+    def self.call(email: nil, username: nil, password:)
+      new(email, username, password).call
     end
 
-    def initialize(username, password)
+    def initialize(email, username, password)
+      @email = email
       @username = username
       @password = password
     end
 
     def call
-      user = User.find_by(username: @username)
+      user =
+        if @email.present?
+          User.find_by(email: @email)
+        elsif @username.present?
+          User.find_by(username: @username)
+        end
+
       return nil unless user
       return nil unless user.authenticate(@password)
       return nil unless user.email_verified?
