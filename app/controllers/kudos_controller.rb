93cb966ba65bf4aa77fb549a679233667bb150d8
Mihle14 <mihlentsaluba14@gmail.com>
2026-01-13 20:00:21 +0200

    controllers

---
+++ app/controllers/kudos_controller.rb
@@ -0,0 +1,25 @@
+class KudosController < ApplicationController
+  before_action :require_login
+
+  def new
+    @kudo = Kudo.new
+    @users = User.where.not(id: current_user.id)
+  end
+
+  def create
+    @kudo = Kudo.new(kudo_params)
+    @kudo.sender = current_user
+
+    if @kudo.save
+      redirect_to dashboard_path, notice: "Kudo sent 🎉"
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def kudo_params
+    params.require(:kudo).permit(:receiver_id, :category, :message)
+  end
+end
