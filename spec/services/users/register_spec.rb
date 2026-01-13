cfbc2ed2c46eb3ab279ce2298d171b6bfc403cb5
Konke Gulwa <konkegulwa02@gmail.com>
2026-01-13 08:54:34 +0200

    Fix Users::Register spec to match async email delivery

---
+++ spec/services/users/register_spec.rb
@@ -0,0 +1,38 @@
+require "rails_helper"
+
+RSpec.describe Users::Register do
+  describe ".call" do
+    let(:params) do
+      {
+        username: "konke",
+        email: "konke@example.com",
+        password: "password123",
+        password_confirmation: "password123"
+      }
+    end
+
+    it "creates a user and sends verification email" do
+      mailer = double(deliver_later: true)
+      allow(UserMailer).to receive(:verify_email).and_return(mailer)
+
+      result = described_class.call(params)
+
+      expect(result.success?).to be true
+
+      user = result.user
+      expect(user).to be_persisted
+      expect(user.email_verified).to be false
+      expect(user.email_verification_token).to be_present
+      expect(user.email_verification_sent_at).to be_present
+
+      expect(UserMailer).to have_received(:verify_email).with(user)
+    end
+
+    it "fails when params are invalid" do
+      result = described_class.call(params.merge(email: ""))
+
+      expect(result.success?).to be false
+      expect(result.error).to be_present
+    end
+  end
+end
