43a5ebfdb8c90c75b410663b605f9e3b834f68c3
Konke Gulwa <konkegulwa02@gmail.com>
2026-01-15 12:02:04 +0200

    Extended dashboard controller to account for profile display

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -2,8 +2,15 @@ class DashboardController < ApplicationController
   before_action :require_login
 
   def index
+    # Feed: all kudos (reverse chronological)
     @kudos = Kudo
-    .includes(:sender, :receiver)
-    .order(created_at: :desc)
-  end 
+      .includes(:sender, :receiver)
+      .order(created_at: :desc)
+
+    # Profile: kudos received by current user
+    @received_kudos = current_user
+      .received_kudos
+      .includes(:sender)
+      .order(created_at: :desc)
+  end
 end
