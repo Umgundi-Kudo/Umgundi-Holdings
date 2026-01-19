8eb99c47ead84cf4e86a1cb00700dc49279f766c
Konke Gulwa <konkegulwa02@gmail.com>
2026-01-19 11:22:59 +0200

    Fix Kudos::KudoCreate success handler

--- app/services/kudos/kudo_create.rb
+++ app/services/kudos/kudo_create.rb
@@ -1,34 +1,33 @@
 module Kudos
-    class KudoCreate
-        def self.call(sender:, params:)
-            new(sender, params).call
-        end
-
-
-        def initialize(sender:, params:)
-            @sender = sender
-            @params = params
-        end
-
-        def call
-            kudo = Kudo.new(@params)
-            kudo.sender = @sender
-
-            if kudo.save
-                success(kudo)
-            else
-                failure(kudo)
-            end
-        end
-
-        private
-
-        def sucess(kudo)
-            OpenStruct.new(success?: true, kudo: kudo)
-        end
-
-        def failure(kudo)
-            OpenStruct.new(success?: false, kudo: kudo)
-        end
+  class KudoCreate
+    def self.call(sender:, params:)
+      new(sender: sender, params: params).call
     end
+
+    def initialize(sender:, params:)
+      @sender = sender
+      @params = params
+    end
+
+    def call
+      kudo = Kudo.new(@params)
+      kudo.sender = @sender
+
+      if kudo.save
+        success(kudo)
+      else
+        failure(kudo)
+      end
+    end
+
+    private
+
+    def success(kudo)
+      OpenStruct.new(success?: true, kudo: kudo)
+    end
+
+    def failure(kudo)
+      OpenStruct.new(success?: false, kudo: kudo)
+    end
+  end
 end
